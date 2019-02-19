1994610fe306cc4e9f1f1c6f40716c95ac0c54ec
dag8 <dag8@pc2-017-l.cs.st-andrews.ac.uk>
2019-02-19 17:44:59 +0000

    Used Jacoco report as a guide to refactor some code and write more tests for GameState.java

--- Hangman-Gradle/src/main/java/cs5031/GameState.java
+++ Hangman-Gradle/src/main/java/cs5031/GameState.java
@@ -29,12 +29,14 @@ public class GameState {
 		this.hintsLeft = hintsLeft;
 	}
 
+	//TODO can make certain methods private if you refactor
+
     /**
      * This function prints the game board in its current state to the console. It prints any letters already
 	 * successfully guessed by the user. A dash is printed in place of any letter not yet guess by the user.
      * @param word a string containing the wordToGuess the user is trying to guess
      */
-	void showCurrentGameBoard(String word) {
+	void showCurrentGameBoard(String word) { 		//TODO may want to consider refactoring this to make it more testable
 		for (int i = 0; i < word.length(); ++i) {
 			if (lettersSuccessfullyGuessed.contains(word.charAt(i)) && word.charAt(i) != ' ') {
 				System.out.print(word.charAt(i));
@@ -47,7 +49,7 @@ public class GameState {
 		System.out.println("");
 	}
 	
-	String guessLetter() {
+	String guessLetter() { 							//TODO may want to consider refactoring this to make it more testable
 		System.out.print("Guess a letter or word to guess (? for a hint): ");
 
 		String str = sc.next();
@@ -55,6 +57,7 @@ public class GameState {
 		return processLetter(str);
 	}
 
+	//TODO insert JavaDOC
 	public String processLetter(String str) {
 		if(str == null || str.length() == 0) {
 			return "error";
@@ -86,24 +89,17 @@ public class GameState {
 		return "incorrect";
 	}
 
-	public Boolean inputLengthGreaterThanOne(String str) {
+	private Boolean inputLengthGreaterThanOne(String str) {
 		if (str.equals(wordToGuess)) {
 			lettersNotGuessedYet.clear();
+			guessesMade++;
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-    /**
-     * This function takes the letter entered by the user and compares it against the set of letters not yet guessed.
-     * If the letter the user guessed is part of lettersNotGuessedYet, it removes it from there and adds it to
-     * lettersSuccessfullyGuess. It then increments guessesMade and returns true. If the letter does not match it
-     * returns false.
-     * @param letter the letter being checked
-     * @return
-     */
-	public Boolean checkGuessedLetter(char letter) {
+	private Boolean checkGuessedLetter(char letter) {
 		for(int i = 0; i < lettersNotGuessedYet.size(); ++i) {
 			if (lettersNotGuessedYet.get(i) == letter) {
 				lettersNotGuessedYet.remove(i);
@@ -116,19 +112,18 @@ public class GameState {
 		return false;
 	}
 
-	//TODO do you even need two functions for this? Should won & lost be combine into one function?
-	boolean won() {
+	boolean won() { 						//TODO may want to consider refactoring this to make it more testable
 		if (lettersNotGuessedYet.size() == 0) return true; else return false;
 	}
 
-	boolean lost() {
+	boolean lost() { 						//TODO may want to consider refactoring this to make it more testable
 		if (lettersNotGuessedYet.size() > 0 && remainingGuesses == 0) return true; else return false;
 	}
 
 	/**
 	 * The hint method offers the user a hint if the user has any hints remaining.
 	 */
-	void hint() { //TODO write a test for this
+	void hint() { 							//TODO may want to consider refactoring this to make it more testable
 		if (hintsLeft == 0) {
 			System.out.println("No more hints allowed");
 		} else {
--- Hangman-Gradle/src/test/java/GameStateTest.java
+++ Hangman-Gradle/src/test/java/GameStateTest.java
@@ -5,17 +5,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class GameStateTest {
-	public static int guesses = 8;
-	public static int hints = 2;
-	public static String testWord = "testy";
-	public static String empty = "";
-	public static String testNull = null;
-	public static String hint = "?";
-	public static String space = " ";
-	public static String incorrectLetter = "g";
-	public static String incorrectWord = "nizzle";
-	public static String correctLetter = "e";
-	public static String correctCapitalized = "S";
+	private static int guesses = 8;
+	private static int hints = 2;
+	private static String testWord = "testy";
+	private static String empty = "";
+	private static String testNull = null;
+	private static String hint = "?";
+	private static String space = " ";
+	private static String incorrectLetter = "g";
+	private static String incorrectWord = "nizzle";
+	private static String correctLetter = "e";
+	private static String correctCapitalized = "S";
 	GameState test;
 
 	@Before
@@ -44,6 +44,10 @@ public class GameStateTest {
 	public void processLetterHint() {
 		assertEquals("hint", test.processLetter(hint));
 		assertEquals(1, test.hintsLeft);
+		assertEquals("hint", test.processLetter(hint));
+		assertEquals(0, test.hintsLeft);
+		assertEquals("hint", test.processLetter(hint));
+		assertEquals(0, test.hintsLeft);
 	}
 
 	@Test
@@ -68,8 +72,32 @@ public class GameStateTest {
 
 	@Test
 	public void processLetterCorrectLetterCapitalized() {
-		assertEquals("correct", test.processLetter(correctCapitalized)); //TODO this test failed
+		assertEquals("correct", test.processLetter(correctCapitalized));
 		assertEquals(1, test.guessesMade);
+		assertEquals(8, test.remainingGuesses);
+	}
+
+	@Test
+	public void processLetterFullWordGuess() {
+		assertEquals("victory", test.processLetter("testy"));
+		assertEquals(1, test.guessesMade);
+		assertEquals(8, test.remainingGuesses);
+	}
+
+	@Test
+	public void processLetterFullWordGuessCapitalized() {
+		assertEquals("victory", test.processLetter("TestY"));
+		assertEquals(1, test.guessesMade);
+		assertEquals(8, test.remainingGuesses);
+	}
+
+	@Test
+	public void processLetterRemainingLettersGuessCorrect() {
+		assertEquals("correct", test.processLetter("t"));
+		assertEquals(1, test.guessesMade);
+		assertEquals(8, test.remainingGuesses);
+		assertEquals("victory", test.processLetter("testy"));
+		assertEquals(2, test.guessesMade);
 		assertEquals(8, test.remainingGuesses);
 	}
 
